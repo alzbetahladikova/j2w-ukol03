6274024353de5b075198cf03eb5652afd28a3e23
alzbetahladikova <beta.strelbova@gmail.com>
2023-10-10 20:27:11 +0200

    vytvoření listu Vizika ve VizitkaController

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,17 +1,27 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+  private final List <Vizitka> seznamVizitek = List.of(
+          new Vizitka("Milena Pokorná","Alfa", "Rovná 100", "100 00", "milena@nemail.com", "111 111 111", "www.nevebovka.cz"),
+          new Vizitka("Alena Pokorná", "Beta", "Křivá 200","200 00","alena@nemail.com", "222 222 222","www.nikdenic.com"),
+          new Vizitka("Jelena Pokorná", "Gama", "Úzká 300","300 00","jelena@nemail.com", "333 333 333","www.prazdnopusto.com")
+   );
+
   @GetMapping("/")
   public String seznam() {
     return "seznam";
   }
+
 
   @GetMapping("/detail")
   public String detail() {
