0357751660bb92042a48852d52b95ac37837af75
alzbetahladikova <beta.strelbova@gmail.com>
2023-10-10 21:58:48 +0200

    vyřešen problém s null hodnotami, ukázka na seznam.ftlh vizitka.firma
    úkol: doplnit do dalších položek

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -16,10 +16,13 @@ public class VizitkaController {
   private final List <Vizitka> seznamVizitek = List.of(
           new Vizitka("Milena Pokorná","Alfa", "Rovná 100", "100 00", "milena@nemail.com", "111 111 111", "www.nevebovka.cz"),
           new Vizitka("Alena Pokorná", "Beta", "Křivá 200","200 00","alena@nemail.com", "222 222 222","www.nikdenic.com"),
-          new Vizitka("Jelena Pokorná", "Gama", "Úzká 300","300 00","jelena@nemail.com", "333 333 333","www.prazdnopusto.com")
+          new Vizitka("Jelena Pokorná", null, "Úzká 300","300 00","jelena@nemail.com", "333 333 333","www.prazdnopusto.com")
    );
 
-
+  //either specify a default value like myOptionalVar!myDefault,
+  // or use [#if myOptionalVar??]when-present[#else]when-missing[/#if].
+  // (These only cover the last step of the expression; to cover the whole expression,
+  // use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
 
   @GetMapping("/")
   public ModelAndView seznam() {
